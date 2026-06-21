93084b84adb684256fa2bdcfb035518a340c691c
KOBRA <joaogabrielsh@gmail.com>
2026-06-21 16:10:39 -0300

    first commit

---
+++ src/main/java/com/kobra/news_ai/NewsAiApplication.java
@@ -0,0 +1,13 @@
+package com.kobra.news_ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewsAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kobra/news_ai/NewsAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kobra.news_ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
